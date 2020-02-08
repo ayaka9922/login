e65fd5d17cfddc2dc164da7c2fba5abec3ec4eda
ayaka9922 <ayk.24a3y@gmail.com>
2020-02-08 19:05:14 +0000

    completed 011

--- app.rb
+++ app.rb
@@ -1,3 +1,45 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require './models'
+
+enable :sessions
+
+get '/' do
+  erb :index
+end
+
+get '/signin' do
+  erb :sign_in
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+post '/signin' do
+  user = User.find_by(mail: params[:mail])
+  if user && user.authenticate(params[:password])
+    session[:user] = user.id
+  end
+  redirect '/'
+end
+
+post '/signup' do
+  if user = User.find_by(mail: params[:mail])
+    erb :already_sign_up
+  else
+   @user = User.create(mail: params[:mail],password: params[:password],
+   password_confirmation: params[:password_confirmation])
+    if @user.persisted?
+      session[:user] = @user.id
+   end
+   redirect '/'
+  end
+end
+
+get '/signout' do
+  session[:user] = nil
+  redirect '/'
+end
